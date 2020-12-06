a2f41a4a0d5198d18b266a272ccf95d65570782c
unknown <vergarajohnbryan@gmail.com>
2020-12-06 10:59:54 +0800

    Applied Delete all

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const ques=document.getElementById("ques");
 const ans=document.getElementById("quesAns");
 const btnSubmit=document.getElementById("btnSubmit");
 const output=document.getElementById("check");
+const btndelete=document.getElementById("btnDel")
 
-btnSubmit.onclick=function addDataFunc(){
-const question = ques.value;
-const answer = ans.value;
+btnSubmit.addEventListener('click',function(){createQAFunc(ques,ans)});
+
+
+function createQAFunc(a,b){
+const question = a.value;
+const answer = b.value;
 localStorage.setItem(question,answer);
-output.innerHTML+=`Question: ${question}<br> Answer:${answer}<hr>`
+output.innerHTML+=`<hr>Question: ${question}<br> Answer:${answer}<br><hr>`;   
+}
+
+btndelete.addEventListener('click',deleteAllQAFunc);
+function deleteAllQAFunc(){
+output.remove();    
 }
