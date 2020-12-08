d629c7ce3834b0c888d4e4d46b59790746051ec0
unknown <vergarajohnbryan@gmail.com>
2020-12-08 18:05:53 +0800

    optimized local storage saving data function

--- app.js
+++ app.js
@@ -3,24 +3,27 @@ const ans=document.getElementById("quesAns");
 const btnSubmit=document.getElementById("btnSubmit");
 const output=document.getElementById("check");
 const btndelete=document.getElementById("btnDel")
-const dataQuesAns=[];
+const dataQues=[];
+const dataAns=[];
 
-btnSubmit.addEventListener('click',function(){createQAFunc(ques,ans)});
+
 btnSubmit.addEventListener('click',function(){saveDataFunc(ques,ans)});
+btnSubmit.addEventListener('click',function(){createQAFunc(ques,ans)});
 btndelete.addEventListener('click',deleteAllQAFunc);
-
-function createQAFunc(a,b){   
-const question = a.value;
-const answer = b.value;
-output.innerHTML+=`<p><hr>Question: ${question}<br> Answer:${answer}<hr></p>`; 
+    
+function createQAFunc(a,b){
+const question=a.value;
+const answer=b.value;
+output.innerHTML+=`<p><hr> Question: ${question} <br> Answer: ${answer}<hr></p>`;
 }
 
 function saveDataFunc(a,b){  
 const question = a.value;
 const answer = b.value;
-const dataDescription="Question and the Answer";
-dataQuesAns.push([{question,answer}]);
-localStorage.setItem(dataDescription,JSON.stringify(dataQuesAns));
+dataAns.push(answer);
+dataQues.push(question);
+localStorage.setItem("Question",JSON.stringify(dataQues));
+localStorage.setItem("Answer",JSON.stringify(dataAns));
 }
 
 function deleteAllQAFunc(){
