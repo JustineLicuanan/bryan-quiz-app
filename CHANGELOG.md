521a725a2d12e5e83c48d3fb61714e89e08e4287
Justine Licuanan <caissoone@gmail.com>
2020-12-08 20:27:42 +0800

    Changes wrote in CHANGELOG.md v0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+## v0.2.0
+
+### index.html:
+
+- Wrap inputs to a form element.
+- Format HTML code using Prettier.
+
+### app.js:
+
+- Change add question event from click to form submit.
+- Merge questions, and answers data to one array.
+
+### readme.txt
+
+- Rename to README.md
+
+### debug.log
+
+- Delete debug.log
+
+### CHANGELOG.md | NEW
+
+- Create CHANGELOG.md
--- app.js
+++ app.js
@@ -1,32 +1,43 @@
-const ques=document.getElementById("ques");
-const ans=document.getElementById("quesAns");
-const btnSubmit=document.getElementById("btnSubmit");
-const output=document.getElementById("check");
-const btndelete=document.getElementById("btnDel")
-const dataQues=[];
-const dataAns=[];
+const addQuestionForm = document.getElementById('add-question-form');
+const output = document.getElementById('check');
+const btndelete = document.getElementById('btnDel');
 
+// State(s)
+const localQuestions = [];
 
-btnSubmit.addEventListener('click',function(){saveDataFunc(ques,ans)});
-btnSubmit.addEventListener('click',function(){createQAFunc(ques,ans)});
-btndelete.addEventListener('click',deleteAllQAFunc);
-    
-function createQAFunc(a,b){
-const question=a.value;
-const answer=b.value;
-output.innerHTML+=`<p><hr> Question: ${question} <br> Answer: ${answer}<hr></p>`;
+addQuestionForm.addEventListener('submit', function (event) {
+	// Prevent unwanted page refresh
+	event.preventDefault();
+
+	saveDataFunc(addQuestionForm.ques.value, addQuestionForm.ans.value);
+	createQAFunc(addQuestionForm.ques.value, addQuestionForm.ans.value);
+
+	// Delete input fields value after creating, and saving to localStorage
+	addQuestionForm.ques.value = '';
+	addQuestionForm.ans.value = '';
+});
+
+btndelete.addEventListener('click', function () {
+	deleteAllQAFunc();
+});
+
+// Save new data to local storage
+function saveDataFunc(question, answer) {
+	const questions = {
+		question, // Same as doing { question: question }
+		answer, // Same as doing { answer: answer }
+	};
+
+	localQuestions.push(questions);
+	localStorage.setItem('questions', JSON.stringify(localQuestions));
 }
 
-function saveDataFunc(a,b){  
-const question = a.value;
-const answer = b.value;
-dataAns.push(answer);
-dataQues.push(question);
-localStorage.setItem("Question",JSON.stringify(dataQues));
-localStorage.setItem("Answer",JSON.stringify(dataAns));
+// Output created data to the DOM
+function createQAFunc(question, answer) {
+	output.innerHTML += `<p><hr> Question: ${question} <br> Answer: ${answer}<hr></p>`;
 }
 
-function deleteAllQAFunc(){
-output.remove(); 
-location.reload();
+// Delete all data in the DOM
+function deleteAllQAFunc() {
+	output.innerHTML = '';
 }
